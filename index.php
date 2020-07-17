1225250bee14455d2c42834fb61585eacd2d3a8f
evgeniy gazetdinov <zplacesound@gmail.com>
2020-07-17 20:17:19 +0300

    work on service block (#3)
    * educate maket
    
    * start on header
    
    * 3 lame blocks
    
    * lame 3 blocks
    
    * 4block
    
    * work on service module

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
 </div>
 <!-- end header+first block place -->
     <div class="container-fluid gray-container">
-          <div class="row  n_content-container">
+          <div class="row  our_specialization-content-container">
                   <div class="col-md-6">
                         <div class="owner-place">
                               <img class="main-granade" src="<?php echo get_template_directory_uri(); ?>/assets/build/img/owner.png" />
                         </div>
                   </div>
                     <div class="col-md-6">
-                          <div class="title-place">
+                          <div class="title-place down-title">
                                 <p class="food-and-drinks">Еда и напитки <span class="other">-</span></p>
                                 <p class='other'>это наша специализация</p>
                           </div>
@@ -47,33 +47,151 @@
             </div>
       </div>
       <div class="container">
-        <div class="row category-container">
+        <div class="row category-content-container">
             <div class="col-md-6">
-                <div class="title-category">
+                <div class="title-category down-title">
                     <p>Категории</p>
                 </div>
             </div>
             <div class="col-md-6">
               <img class="main-category" src="<?php echo get_template_directory_uri(); ?>/assets/build/img/category.png" />
             </div>
-            <div class="row">
-                  <div class="col-md-5">
+                  <div class="col-md-6">
                           <div class="under-title">
-                              <p>На данном этапе мы сосредоточены на продвижении определенной категории товаров и услуг, где были достигнуты наилучшие результаты и показатели продаж, а именно:</p>
+                              <p>На данном этапе мы сосредоточены на продвижении определенной  категории  товаров и услуг, где были достигнуты наилучшие результаты и показатели продаж, а именно:</p>
                           </div>
                   </div>
             </div>
-            <div class="row">
                   <div class="col-md-5">
-                        <ul>
-                          <li class="position">Продукты питания и напитки</li>
-                          <li class="position">Доставка еды</li>
-                          <li class="position">Кафе и рестораны</li>
+                        <ul class="list-with-categories" style="list-style-image: url('<?php echo get_template_directory_uri(); ?>/assets/build/img/vector.png')">
+                              <li class="position">Продукты питания и напитки</li>
+                              <li class="position">Доставка еды</li>
+                              <li class="position">Кафе и рестораны</li>
                         </ul>
                   </div>
-            </div>
         </div>
+      <div class="container-fluid gray-container">
+            <div class="row our-advantage-content-container">
+                <div class="col-md-8 offset-md-2">
+                    <div class="out-advantage__title down-title">
+                      <p>Наши преимущества</p>
+                    </div>
+                </div>
+            <div class="row grid-width-container">
+                <div class="col-md-6">
+                    <div class="row">
+                          <div class="col-md-5">
+                              <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/one.png" />
+                          </div>
+                          <div class="col-md-7">
+                              <p class="advantage___number__title">Профессионализм по региональной стоимости</p>
+                              <p class="advatage__number__text">Делаем свою работу, находясь при этом в регионах и работая удаленно, что снижает стоимость услуг, не теряя качество P.S. до Москвы добираемся за 4 часа</p>
+                          </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                          <div class="col-md-5">
+                              <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/two.png" />
+                          </div>
+                          <div class="col-md-7">
+                              <p class="advantage___number__title">Концентрация</p>
+                              <p class="advatage__number__text">Сфокусированы на конкретной нише, что дает нам преимущество в этой области</p>
+                          </div>
+                  </div>
+                </div>
+            </div>
+            <div class="row grid-width-container">
+                <div class="col-md-6">
+                    <div class="row">
+                          <div class="col-md-5">
+                              <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/three.png" />
+                          </div>
+                          <div class="col-md-7">
+                              <p class="advantage___number__title">Гарантии</p>
+                              <p class="advatage__number__text">Исходя из своего опыта, можем заранее спрогнозировать примерные показатели по охвату, заявкам, вовлеченности и т.д.</p>
+                          </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                          <div class="col-md-5">
+                              <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/four.png" />
+                          </div>
+                          <div class="col-md-7">
+                              <p class="advantage___number__title">Работа с 3D-графикой</p>
+                              <p class="advatage__number__text">Ведем проекты в социальных сетях с применением моушен-дизайна, что делает ваш бренд запоминающимся и выделяющимся на фоне остальных</p>
+                          </div>
+                  </div>
+                </div>
+            </div>
+          </div>
       </div>
+      <div class="container services-content-container">
+            <div class="row grid-width-container">
+                  <div class="col-md-8 offset-md-2">
+                        <p class="services__title down-title">
+                          Услуги
+                        </p>
+                  </div>
+            </div>
+            <div class="row grid-width-container">
+
+                          <div class="col-md-4 one-card">
+                                  <div class="row">
+                                      <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/strategy.png"
+                                  </div>
+                                  <div class="row">
+                                    <div class="col-md-12">
+                                    Стратегия продвижения
+                                    </div>
+                                  </div>
+                                  <div class="row">
+                                    <div class="col-md-12">
+                                      Разработка стратегии продвижения в социальных сетях: вконтакте, фейсбук, инстаграм, яндекс дзен
+                                    </div>
+                                  </div>
+                          </div>
+                          </div>
+
+                          <div class="col-md-4">
+                              <div class="row">
+                                <div class="col-md-12">
+                                  <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/content.png"
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-12">
+                                  Генерация контента
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-12">
+                                  Генерация и ведение уникального контента, фотосессии
+                                </div>
+                              </div>
+                          </div>
+                          </div>
+                          <div class="col-md-4">
+                              <div class="row">
+                                <div class="col-md-12">
+                                  <img src="<?php echo get_template_directory_uri(); ?>/assets/build/img/reklama.png"
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-12">
+                                    Реклама
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-12">
+                                  Настройка и запуск таргетированной рекламы
+                                </div>
+                              </div>
+                              </div>
+                      </div>
+
+
 
 </body>
 <?php get_footer()?>
