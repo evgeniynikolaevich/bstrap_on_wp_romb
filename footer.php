893b613adab13202d66b5d8fff3ed11c254e85e2
evgeniy_nikolaevich <zplacesound@gmail.com>
2020-07-28 19:29:37 +0300

    with footer placing

--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
 <footer>
-  <div class="container-fluid   footer-container">
-    <div class="row grid">
-              <div class="col-md-2">
+  <div class="container-fluid   footer-container footer-container-mobile">
+    <div class="row grid footer-mobile">
+              <div class="col-md-2 col order-1">
                   <p class="footer-logo">Горн </p>
               </div>
-              <div class="col-md-3 ">
+              <div class="col-md-3 col order-last">
                       <div class="creditails">
                         <p>ИП Баранов Петр Иванович</p>
                         <p>ИНН: 3200 06918927</p>
                     </div>
               </div>
-              <div class="col-md-2 offset-md-1">
+              <div class="col-md-2 offset-md-1 order-4 col">
                     <div class="social">
                       <a href="https://vk.com/" style="background: url(<?php echo get_template_directory_uri(); ?>/assets/build/img/social.png)" target="_blank"class="vk"></a>
                       <a href="https://web.facebook.com/?_rdc=1&_rdr"  style="background: url(<?php echo get_template_directory_uri(); ?>/assets/build/img/social.png);background-position:70px;"target="_blank" class="fb"></a>
                       <a href="https://telegram.org/"  style="background: url(<?php echo get_template_directory_uri(); ?>/assets/build/img/social.png);background-position:140px;"target="_blank" class="tg"></a>
                     </div>
                 </div>
-              <div class="col-md-2">
+              <div class="col-md-2 col order-2">
                 <p class="number">
                   +7 980 336 10 02
                 </p>
               </div>
-              <div class="col-md-2 ">
+              <div class="col-md-2 col order-3">
                 <div class="call-me callback-footer">
                       <div class="callback ">
                           <a href="#" class="callback__button">Обратный звонок</a>
